86325a503eef148c018fc4bca77f2c6c88729b2c
Muddasar Hassan <mudsr.hsn@gmail.com>
2025-03-03 03:36:38 +0500

    backend updated & terraform init added into workflow

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tf-az-backend-rg"
     storage_account_name = "tfazbackend123"
-    container_name       = "tf-az-tfstate"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
     subscription_id      = "256c47d7-6c5f-4e92-b116-d234771e3690"
   }
