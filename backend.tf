67186e9aaf246265da23f3eb10ff3fa3cc7ff6e2
Muddasar Hassan <mudsr.hsn@gmail.com>
2025-03-03 03:12:10 +0500

    remove secret from commit history

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tf-az-backend-rg"
-    storage_account_name  = "tfazbackend123"
-    container_name        = "tf-az-tfstate"
-    key                   = "terraform.tfstate"
+    resource_group_name  = "tf-az-backend-rg"
+    storage_account_name = "tfazbackend123"
+    container_name       = "tf-az-tfstate"
+    key                  = "terraform.tfstate"
     subscription_id      = "256c47d7-6c5f-4e92-b116-d234771e3690"
   }
 }
