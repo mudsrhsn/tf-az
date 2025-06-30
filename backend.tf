aa9cba1b8e090e0de8639ccaf60bee4701b0f7c6
Muddasar Hassan <mudsr.hsn@gmail.com>
2025-06-30 14:45:08 +0300

    subscription id edited

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "tfazbackend123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    subscription_id      = "256c47d7-6c5f-4e92-b116-d234771e3690"
+    subscription_id      = "256c47d7-6c5f-4e92-b116-d23471e3690"
   }
 }
